c1da7d9dc07c9e1dc0a07a81f9b31f13eda8f03f
MattTsoo <tsomy@uci.edu>
2025-11-16 16:04:16 -0500

    Created individual tyre set tracking and condition updating based off tyre degredation and laps used

--- Back_End/api_key.py
+++ Back_End/api_key.py
@@ -1,2 +1,2 @@
 API_KEY = "b6b414bda24544b3ac5184659250111"
-DB_FILE = "/AI model/weather.db"
+DB_FILE = "/Users/matthewtso/F1 Project/AI model/weather.db"
--- Back_End/tyre_organizer.py
+++ Back_End/tyre_organizer.py
@@ -1,4 +1,8 @@
-class SessionType:
+from dataclasses import dataclass
+from typing import List
+from enum import Enum
+
+class SessionType(Enum):
     FP1 = "Free Practice 1"
     FP2 = "Free Practice 2"
     FP3 = "Free Practice 3"
@@ -7,7 +11,7 @@ class SessionType:
     QUALIFYING = "Qualifying"
     RACE = "RACE"
 
-class TireCompound:
+class TireCompound(Enum):
     SOFT = "Soft"
     MEDIUM = "Medium"
     HARD = "HARD"
@@ -20,3 +24,34 @@ class TireAllocation:
         self.medium = medium
         self.hard = hard
 
+@dataclass
+class TireSet:
+    """
+    Individual tire set's with usage tracking
+    """
+    compound: TireCompound
+    set_number: int
+    laps_used = int = 0
+    sessions_used: List[SessionType] = None
+    condition: float = 100.0
+
+    def __post_init__(self):
+        if self.sessions_used is None:
+            self.sessions_used = []
+
+    def use(self, laps: int, session: SessionType):
+        """
+        Update tire condition after usage
+        """
+        self.laps_used += laps
+        if session not in self.sessions_used:
+            self.sessions_used.append(session)
+
+        deg_rate = {
+            TireCompound.SOFT: 2.5,
+            TireCompound.MEDIUM: 1.5,
+            TireCompound.HARD: 1.0
+        }
+
+        self.condition -= deg_rate.get(self.compound, 1.5) * laps
+        self.condition = max(0, self.condition)
